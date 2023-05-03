a5a1ab94cebd6277f880498fb13db39cfd9a4893
WhiteFlaw <71926259+WhiteFlaw@users.noreply.github.com>
2023-05-03 12:38:21 +0800

    node 读写excel

--- Nodejs Sec/readExcel/index.js
+++ Nodejs Sec/readExcel/index.js
@@ -1,11 +1,13 @@
-
+let fs = require('fs');
 const xlsx = require("node-xlsx");
 
-const path = './四月工资总汇.xls';
-const day_total = 30; // 天数/月
-const price = 160; // 单价
+const input = './数据/四月工资总汇.xls';
+const output = './结果/四月工资总汇.xls';
 
-const list = xlsx.parse(path);
+const price = 160; // 单价
+const day_total = 30; // 天数/月
+
+const list = xlsx.parse(input);
 const rawData = list[0].data;
 
 const data = [];
@@ -20,6 +22,7 @@ function formatter() {
             }
         }
     })
+    console.log('formatted.')
 }
 
 function getStable() { // 整理前两名数据
@@ -41,37 +44,82 @@ function getDaily() { // 整理计件数据
         }
         num += 1;
     }
+    console.log('restructure finished.')
 }
 
 function getTotalYield() { // 总产量
+    console.log('computed.')
+    for (let i = 0; i < 3; i++) {
+        data[i]['总工资'] = data[i]['日工'].reduce((prev, curr) => {
+            if (isNaN(Number(curr))) {
+                return prev + 0;
+            } else {
+                return prev + Number(curr);
+            }
+        },)
+    }
+
     for (let i = 2; i < data.length; i++) {
         data[i]['总产量'] = data[i]['产量'].reduce((prev, curr) => {
             if (isNaN(Number(curr))) {
                 return prev + 0;
             } else {
-                return prev + curr;
+                return prev + Number(curr);
             }
         }, 0)
-    }
-}
-
-function getSalary() { // 总日工
-    for (let i = 0; i < data.length; i++) {
-        data[i]['总日工'] = data[i]['日工'].reduce((prev, curr) => {
-            if (isNaN(Number(curr))) {
-                return prev + 0;
-            } else {
-                return prev + curr;
-            }
-        }, 0)
-    }
-}
-
-function somethingElse() {
-    for(let i = 2; i < data.length; i++) {
         data[i]['总产值'] = data[i]['总产量'] * price;
-        data[i]['工资'] = data[i]['总日工'] + data[i]['总产值'];
     }
+}
+
+
+function write() {
+    console.log('writing...')
+    let excelData = [
+        ['', data[0].name, data[1].name],
+        ['', '', '']
+    ];
+
+    for (let i = 2; i < data.length; i++) { // 表头
+        excelData[0].push(data[i].name);
+        excelData[0].push('');
+        if (i < data.length - 1) {
+            excelData[1].push('日工');
+            excelData[1].push('产量');
+        }
+    }
+
+    for (let i = 0; i < day_total; i++) { // 表体
+        excelData.push([i + 1, data[0]['日工'][i], data[1]['日工'][i]]);
+        for (let j = 0; j < data.length - 2; j++) {
+            excelData[2 + i].push(data[2 + j]['日工'][i]);
+            excelData[2 + i].push(data[2 + j]['产量'][i]);
+        }
+    }
+
+    console.log(data)
+
+    const temArr0 = ['总日工/总产量', data[0]['总工资'], data[1]['总工资']];
+    const temArr1 = ['总产值', '', ''];
+
+    for (let i = 2; i < data.length; i++) {
+        temArr0.push(data[i]['总日工'], data[i]['总产量']);
+        temArr1.push('', data[i]['总产值']);
+    }
+
+    excelData.push([], temArr0, temArr1);
+
+    const excelArr = [{
+        name: `sheet1`,
+        data: excelData
+    }];
+
+    // xlsx转化成二进制
+    let buffer = xlsx.build(excelArr);
+    fs.writeFile(output, buffer, function (err) {
+        if (err)
+            throw err;
+        console.log('done.');
+    });
 }
 
 formatter();
@@ -82,8 +130,4 @@ getDaily();
 
 getTotalYield();
 
-getSalary();
-
-somethingElse();
-
-console.log(data);
+write();
